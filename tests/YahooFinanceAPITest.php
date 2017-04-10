e6015f890a24fb71316215dc3d4cd6b7421f05a2
Jesse Griffin <hemham914@gmail.com>
2017-04-10 16:18:15 -0700

    Added composer installation for dependency management.
    Added phpunit library to project.
    
    Added tests/ directory for phpunit tests.
    
    Created new class YahooFinanceAPI to handle updating the current market
    rate with the data available on yahoo finance.
    
    Created new test case for YahooFinanceAPI.

---
+++ tests/YahooFinanceAPITest.php
@@ -0,0 +1,12 @@
+<?php
+
+include '../lib/autoload.php';
+include '../vendor/autoload.php';
+include '../helpers/processSettings.php';
+include '../helpers/OptionsPage.php';
+//include 'lib/calcNaav.php';
+
+class YahooFinanceAPITest extends PHPUnit_Framework_TestCase
+{
+    
+}
